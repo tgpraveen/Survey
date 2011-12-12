123e05c257aac3f2eea7163f1fbe9e510296ac9e
Praveen Thirukonda <tgpraveen89@gmail.com>
2011-12-11 21:35:14 -0500

    create1 done.

--- main.py
+++ main.py
@@ -23,31 +23,40 @@ class MainPage(webapp2.RequestHandler):
         if user:
             greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
                         (user.nickname(), users.create_logout_url("/")))
-            self.response.out.write("<html><body>%s</body></html>" % greeting)
+            self.response.out.write("<html><body>%s" % greeting)
             
             self.response.out.write("""<br><br><br><br><br><br><div style="float:left;text-align: center;background-image: url('/stylesheets/rounded_fixed.gif'); width: 228px; height: 160px; padding: 10px;">
-            <a href='/create'><h3>Create a new survey.</h3></a></div></style>""")
+            <a href='/create1'><h3>Create a new survey.</h3></a></div></style>""")
 
             self.response.out.write("""<div style="float:left;text-align: center;background-image: url('/stylesheets/rounded_fixed.gif'); width: 228px; height: 160px; padding: 10px;">
             <a href='/edit'><h3>Edit surveys created by you.</h3></a></div>""")
 
             self.response.out.write("""<div style="float:left;text-align: center;background-image: url('/stylesheets/rounded_fixed.gif'); width: 228px; height: 160px; padding: 10px;">
             <a href='/vote'><h3>Vote on/View results of the surveys of all the users.</h3></a></div>""")
-     
+            self.response.out.write("</body></html>") 
         else:
             self.redirect(users.create_login_url(self.request.uri))
         
-class Create(webapp2.RequestHandler):
+class Create1(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
             greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
                         (user.nickname(), users.create_logout_url("/")))
-            self.response.out.write("<html><body>%s</body></html>" % greeting)
+            self.response.out.write("<html><body>%s<br><br><br><br><br><br></body></html>" % greeting)
+            self.response.out.write("""<form name=createform1 action="/create2" method="post">
+            Enter the survey name:- <input type="text" name=surveyname>
+            <br>Will you be using images as any of your survey answers? <input type=radio name=useimages value="n" checked> No
+            <input type=radio name=useimages value="y"> Yes
+            <br>Number of questions? <input type="text" name=noofques>
+            <br>Number of options for each question? <input type="text" name=noofoptionsperques>
             
+            
+            <br><br><input type="submit" value="Create Survey"><input type="reset" value="Clear Form"></form>""")
+            self.response.out.write("</body></html>")
         else:
             self.redirect(users.create_login_url(self.request.uri))
             
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/create', Create)],
+                               ('/create1', Create1)],
                               debug=True)
