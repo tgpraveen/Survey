30fc8afc93e0212089150eba0d12c060287c0373
Praveen Thirukonda <tgpraveen89@gmail.com>
2011-12-11 21:49:19 -0500

    Create2 work started.

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class Create1(webapp2.RequestHandler):
         if user:
             greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
                         (user.nickname(), users.create_logout_url("/")))
-            self.response.out.write("<html><body>%s<br><br><br><br><br><br></body></html>" % greeting)
+            self.response.out.write("<html><body>%s<br><br><br><br><br><br>" % greeting)
             self.response.out.write("""<form name=createform1 action="/create2" method="post">
             Enter the survey name:- <input type="text" name=surveyname>
             <br>Will you be using images as any of your survey answers? <input type=radio name=useimages value="n" checked> No
@@ -56,7 +56,21 @@ class Create1(webapp2.RequestHandler):
             self.response.out.write("</body></html>")
         else:
             self.redirect(users.create_login_url(self.request.uri))
+
+class Create2(webapp2.RequestHandler):
+    def post(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
+                        (user.nickname(), users.create_logout_url("/")))
+            self.response.out.write("<html><body>%s<br><br><br><br><br><br>" % greeting)
+            self.response.out.write('Creating Survey:- ')
+            self.response.out.write(cgi.escape(self.request.get('surveyname')))
+            self.response.out.write("</body></html>")
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
             
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/create1', Create1)],
+                               ('/create1', Create1),
+                               ('/create2', Create2)],
                               debug=True)
