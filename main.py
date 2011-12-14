4ef53a257862f857d030ee81900530cdcd5f4e44
Praveen Thirukonda <tgpraveen89@gmail.com>
2011-12-14 04:48:50 -0500

    Done till Edit2

--- main.py
+++ main.py
@@ -40,10 +40,13 @@ class MainPage(webapp2.RequestHandler):
             <a href='/create1'><h3>Create a new survey.</h3></a></div></style>""")
 
             self.response.out.write("""<div style="float:left;text-align: center;background-image: url('/stylesheets/rounded_fixed.gif'); width: 228px; height: 160px; padding: 10px;">
-            <a href='/edit'><h3>Edit surveys created by you.</h3></a></div>""")
+            <a href='/edit1'><h3>Edit surveys created by you.</h3></a></div>""")
   
             self.response.out.write("""<div style="float:left;text-align: center;background-image: url('/stylesheets/rounded_fixed.gif'); width: 228px; height: 160px; padding: 10px;">
-            <a href='/vote'><h3>Vote on/View results of the surveys of all the users.</h3></a></div>""")
+            <a href='/vote'><h3>Vote on the surveys of all the users.</h3></a></div>""")
+            
+            self.response.out.write("""<div style="float:left;text-align: center;background-image: url('/stylesheets/rounded_fixed.gif'); width: 228px; height: 160px; padding: 10px;">
+            <a href='/result1'><h3>View results of the surveys.</h3></a></div></style>""")
             self.response.out.write("</body></html>") 
         else:
             self.redirect(users.create_login_url(self.request.uri))
@@ -258,7 +261,7 @@ class Vote3(webapp2.RequestHandler):
                 currentvote.chosenoption=self.request.get(oq)
                 currentvote.put()
                 j=j+1
-            #self.response.out.write("Voting done.")
+            self.response.out.write("<br>Voting done.<br><a href='/'> Click here to go to main menu.</a>")
             
             
 
@@ -266,8 +269,240 @@ class Vote3(webapp2.RequestHandler):
             self.response.out.write("</body></html>")
         else:
             self.redirect(users.create_login_url(self.request.uri))
+
+class Result1(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
+                        (user.nickname(), users.create_logout_url("/")))
+            self.response.out.write("<html><body>%s<br><br><br><br><br><br>" % greeting)
+            surveylist = db.GqlQuery("SELECT * "
+                                "FROM SurveyList")
+            self.response.out.write("<h2>Viewing Results</h2>")
+            self.response.out.write("<center>Please select from one of the following surveys:-")
+            #self.response.out.write("<form name=voteform1 action='/voteform2'>")
+            for cntr in surveylist:
+              self.response.out.write("<br><a href='/result2?surveyname=%s'> %s </a>" % (cntr.name,cntr.name))
+            self.response.out.write("</center>")            
+            self.response.out.write("</body></html>")
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+class Result2(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
+                        (user.nickname(), users.create_logout_url("/")))
+            self.response.out.write("<html><body>%s<br><br><br><br><br><br>" % greeting)
+            self.response.out.write("<div align=right><br><a href='/'> Click here to go to main menu.</a></div>")
+            self.response.out.write("<h2>Viewing Results</h2>")
+            surveyname=self.request.get('surveyname')
+            questionlist = db.GqlQuery("SELECT * "
+                                "FROM Survey "
+                                "WHERE surveyid=:1 ", surveyname)
+            votelist = db.GqlQuery("SELECT * "
+                                "FROM Votes "
+                                "WHERE surveyid=:1 ", surveyname)
+            self.response.out.write("<br>Viewing survey:- %s" % surveyname)
+            #votescollection[0]=0
+            #votescollection = []
+            for ques in questionlist:
+              self.response.out.write("<br><br>Q:- %s" % ques.question)
+              #for cntr in range(1,len(ques.options)+1):
+              #  votescollection[cntr]=0
+              #for cntr in ques.options:
+              #  votescollection[cntr]=0
+              i=0
+              for crntoption in ques.options:
+                #i=1
+                votescollection = []
+                for crntvote in votelist:
+                  #self.response.out.write("crntvote is :- %s" % crntvote.question)
+                  #self.response.out.write("ques is :- %s" % ques.question)
+                  if crntvote.question==ques.question and crntvote.chosenoption==crntoption:
+                      votescollection.append(1)
+                self.response.out.write("<br>%s %s" % (crntoption, len(votescollection)))
+                i+=1
+              
             
+            votecount=0
+
             
+            self.response.out.write("</body></html>")
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+class Edit1(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
+                        (user.nickname(), users.create_logout_url("/")))
+            self.response.out.write("<html><body>%s<br><br><br><br><br><br>" % greeting)
+            surveylist = db.GqlQuery("SELECT * "
+                                "FROM SurveyList")
+            self.response.out.write("<h2>Selecting Survey to edit.</h2>")
+            self.response.out.write("<center>Please select from one of the following surveys:-")
+            #self.response.out.write("<form name=voteform1 action='/voteform2'>")
+            for cntr in surveylist:
+              self.response.out.write("<br><a href='/edit2?surveyname=%s'> %s </a>" % (cntr.name,cntr.name))
+            self.response.out.write("</center>")            
+            self.response.out.write("</body></html>")
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+class Edit2(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
+                        (user.nickname(), users.create_logout_url("/")))
+            self.response.out.write("<html><body>%s<br><br><br><br><br><br>" % greeting)
+            self.response.out.write("<div align=right><br><a href='/'> Click here to go to main menu.</a></div>")
+            #self.response.out.write("<h2>Viewing Results</h2>")
+            surveyname=self.request.get('surveyname')
+            questionlist = db.GqlQuery("SELECT * "
+                                "FROM Survey "
+                                "WHERE surveyid=:1 ", surveyname)
+            #self.response.out.write("<br>Viewing survey:- %s" % surveyname)
+
+
+            #useimages=cgi.escape(self.request.get('useimageshidden'))
+            useimages="n"
+            #noofques1=db.GqlQuery("SELECT count(question) "
+            #                    "FROM Survey "
+            #                    "WHERE surveyid=:1 ", surveyname)
+            noofques=0
+            for cnts in questionlist:
+              noofques+=1
+            self.response.out.write("Hey folks! %s" % noofques)
+            for abcd in questionlist:
+              noofoptions=int(len(abcd.options))
+              break
+            #self.response.out.write("<br>Hi %sabc" % useimages)
+
+
+            dbsurveys = db.GqlQuery("SELECT * "
+                            "FROM SurveyList ")
+            
+            #for cntr in dbsurveys:
+            #  if cntr.name == surveyname:
+            #    self.response.out.write("<h3>That Survey name --> <b>'%s'</b> has not been created yet, so can't be modified.</h3>" % surveyname)
+            #    self.response.out.write("<FORM><INPUT TYPE='button' VALUE='Back' onClick='history.go(-1);return true;'></FORM>")
+            #    return;
+                
+            #newsurvey=SurveyList(creator = users.get_current_user(),name=surveyname)
+            #newsurvey.put()
+            #self.response.out.write("Done")
+            
+            self.response.out.write('Modifying Survey:- ')
+            self.response.out.write(cgi.escape(self.request.get('surveyname')))
+            #noofques = int(cgi.escape(self.request.get('noofques')))
+            #noofoptions = int(cgi.escape(self.request.get('noofoptionsperques')))
+            #useimages = cgi.escape(self.request.get('useimages'))
+            self.response.out.write("""<form name=editform3 action="/edit3" enctype="multipart/form-data" method="post">""")
+            #if noofques==6:
+             # self.response.out.write(range(noofques))
+            #else:
+             # self.response.out.write("Hey"+noofques)
+            qstnlst = db.GqlQuery("SELECT * "
+                                "FROM Survey "
+                                "WHERE surveyid=:1 ", surveyname)
+            question9 = []
+            optionslist = []
+            for qcntr in qstnlst:
+              question9.append(qcntr.question)
+            for qcntr1 in qstnlst:
+              for xyz in range(noofoptions):
+                  optionslist.append(qcntr1.options[xyz])
+              #for qwe in range(quesno)
+                #if qwe==quesno:
+            i=0
+            self.response.out.write("<h5> For those options where you are going to use images, don't write anything in the textbox AND upload the file using 'Choose file' option.</h5>")
+            for quesno in range(1,noofques+1):
+              self.response.out.write("""Question %s:- <input type=text name=question%s value=%s><br>""" % (quesno,quesno,question9[quesno-1]))                        
+              if useimages=="n":                        
+                for optionno in range(1,noofoptions+1):
+                  self.response.out.write("""Option %s:- <input type=text name=q%soption%s value=%s><br>""" % (optionno,quesno,optionno,optionslist[i]))
+                  i+=1
+                self.response.out.write("<br><br>")
+              elif useimages=="y":
+                for optionno in range(1,noofoptions+1):
+                  self.response.out.write("""<pre>Option %s:- <input type=text name=q%soption%s>            <input type="file" name=q%soptionfile%s/><br></pre>""" % (optionno,quesno,optionno,quesno,optionno))
+                self.response.out.write("<br><br>")
+            self.response.out.write("""<input type=hidden name=surveynamehidden value='%s'>""" % (surveyname))
+            self.response.out.write("""<input type=hidden name=useimageshidden value=%s>""" % useimages)
+            self.response.out.write("""<input type=hidden name=noofoptionshidden value=%s>""" % noofoptions)
+            self.response.out.write("""<input type=hidden name=noofqueshidden value=%s>""" % noofques)
+            self.response.out.write("""<br><br><input type="submit" value="Modify Survey"><input type="reset" value="Clear Form"></form>""")
+
+            
+            self.response.out.write("<h3>Survey modified. <a href='/'>Click here to go back.</a></h3>")
+            self.response.out.write("</body></html>")
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+class Edit3(webapp2.RequestHandler):
+    def post(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
+                        (user.nickname(), users.create_logout_url("/")))
+            self.response.out.write("<html><body>%s<br><br>" % greeting)
+            self.response.out.write('Creating Survey:- ')
+            surveyname=cgi.escape(self.request.get('surveynamehidden'))
+            self.response.out.write(surveyname)
+            #surveykeylist=db.GqlQuery("SELECT * "
+            #                "FROM SurveyList "
+            #                "WHERE name=:1",surveyname)
+            #for cntr in surveykeylist:
+            #  currentsurveykey=cntr.key()
+               #self.response.out.write("%s" % cntr.key())
+
+            useimages=cgi.escape(self.request.get('useimageshidden'))
+            noofques=int(cgi.escape(self.request.get('noofqueshidden')))
+            noofoptions=int(cgi.escape(self.request.get('noofoptionshidden')))
+            #self.response.out.write("<br>Hi %sabc" % useimages)
+            for cntr in range(1,noofques+1):
+              currentquesno="question" + str(cntr)
+              surveyquestion=Survey(question=cgi.escape(self.request.get(currentquesno)),surveyid=surveyname)
+              if useimages == "n":                  
+                  for innercntr in range(1,noofoptions+1):
+                      currentoption="q"+str(cntr)+"option" + str(innercntr)
+                      #self.response.out.write("<br>Hi %s" % currentoption)
+                      surveyquestion.options.append(cgi.escape(self.request.get(currentoption)))
+                  #self.response.out.write("<br>eoq")
+                  surveyquestion.put()
+              elif useimages == "y":
+                for innercntr in range(1,noofoptions+1):
+                      currentoption="q"+str(cntr)+"option" + str(innercntr)
+                      surveyquestion.options.append(cgi.escape(self.request.get(currentoption)))
+                      if cgi.escape(self.request.get(currentoption)) != "":
+                        surveyquestion.options.append(cgi.escape(self.request.get(currentoption)))
+                      else:
+                        currentoptionfile="q"+cntr+"optionfile" + str(innercntr)
+                        optionpic = self.request.get(currentoptionfile)
+                        surveyquestion.optionpic = db.Blob(optionpic)
+                #self.response.headers['Content-Type'] = "image/png"
+                #self.response.out.write(surveyquestion.optionpic)
+                surveyquestion.put()
+
+
+                pict = db.GqlQuery("SELECT * "
+                                "FROM Survey ")
+                for pictcntr in pict:
+                    self.response.out.write("<div><img src='/img1?img_id=%s'></img></div>" % pictcntr.key())
+                    #Not working above inserting/showing pics.        
+                    #greet = db.get(self.request.get(pictcntr.key()))
+                    #self.response.out.write(greet.question)
+            self.response.out.write("<h3>Survey created. <a href='/'>Click here to go back.</a></h3>")
+            self.response.out.write("</body></html>")
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+            
+    
             
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/create1', Create1),
@@ -276,5 +511,10 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                ('/img1', Image1),
                                ('/vote', Vote),
                                ('/vote2', Vote2),
-                               ('/vote3', Vote3)],
+                               ('/vote3', Vote3),
+                               ('/result1', Result1),
+                               ('/result2',Result2),
+                               ('/edit1',Edit1),
+                               ('/edit2',Edit2),
+                               ('/edit3',Edit3)],
                               debug=True)
